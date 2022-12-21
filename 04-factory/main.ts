ca0a350bfc700ed6639f2d5d9414bb7945a446c4
benjilli <benjilli@tencent.com>
2022-12-21 11:59:06 +0800

    doc: 完善部分注释

--- 04-factory/main.ts
+++ 04-factory/main.ts
@@ -1,15 +1,20 @@
+/**
+ * 工厂模式：提供了一种创建对象的最佳方式。
+ * 根据传递的参数，来决定最终返回什么样的对象
+ */
 class IOSButton {}
 class AndroidButton {}
 
 // without factory
-// 使用三元运算符
+// 不用工厂模式，使用三元运算符
 let os1 = 'ios';
 let os2 = 'android';
-const btn1 = os1 === 'ios' ? new IOSButton() : new AndroidButton()
-const btn2 = os2 === 'ios' ? new IOSButton() : new AndroidButton()
+const btn1 = os1 === 'ios' ? new IOSButton() : new AndroidButton();
+const btn2 = os2 === 'ios' ? new IOSButton() : new AndroidButton();
 
 // with factory
-// 提高内聚，降低耦合
+// 抽象一个类，职责是：
+// 返回合适的实例，即提高内聚，又降低耦合
 class ButtonFactory {
   createButton(os: string) {
     if (os === 'ios') {
